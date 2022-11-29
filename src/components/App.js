45efd45d260369233c1915eb045bc9a1555e7a5e
Kevin Diesenberg <kdiesenb@gmail.com>
2022-11-29 13:37:23 -0500

    re did where useEffect lives on initial fetch.  felt like i cornered myself in to a wrong component

--- src/components/App.js
+++ src/components/App.js
@@ -11,22 +11,13 @@
   correctIndex: 1
 },
 */
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
-  const [quizObj, setQuizObj] = useState([]);
-
-
-  useEffect(() => {
-    fetch("http://localhost:4000/questions")
-      .then(res => res.json())
-    .then(quizObj => setQuizObj(quizObj))
-  }, [])
-
 
   return (
     <main>
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 
 function QuestionList() {
+
+  const [quizObj, setQuizObj] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((quizObj) => setQuizObj(quizObj));
+  }, []);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
